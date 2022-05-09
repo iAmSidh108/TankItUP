cde1e85871e687c7056ef55528c088181a0f737c
Siddharth Tiwari <sidh589@gmail.com>
2022-05-10 00:21:48 +0530

    Added Tank in the scene and added script to spawn tank to new location each time we play.

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    public Transform[] spawnPoints;
+    void Start()
+    {
+        //
+        foreach (Transform sPoint in spawnPoints)
+        {
+            sPoint.gameObject.SetActive(false);
+        }
+
+        Transform newTrans = GetSpawnPoints();
+        Instantiate(tankPrefab, newTrans.position, newTrans.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Transform GetSpawnPoints()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)];
+    }
+}
