9441785fcc8784a8f2ef8be1dc4abbc537239531
Siddharth Tiwari <sidh589@gmail.com>
2022-05-10 02:09:34 +0530

    Added Movement and Rotation to the tank.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,19 +1,35 @@
 ﻿
 using UnityEngine;
 
-public class TankController 
+public class TankController
 {
     private TankView tankView;
     private TankModel tankModel;
 
+    private Rigidbody rb;
+
     public TankController(TankView _tankView, TankModel _tankModel)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+
+        rb = tankView.GetRigidBody();
+
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
+    }
 
-        GameObject.Instantiate(tankView.gameObject);
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movement * movementSpeed;
+    }
+
+    public void Rotate(float rotate,float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotate * rotateSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector *Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
     }
     
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
+
+    float movement;
+    float rotation;
+
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +19,29 @@ public class TankView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        if (movement != 0)
+        {
+            tankController.Move(movement,30);
+        }
+        if (rotation != 0)
+        {
+            tankController.Rotate(rotation, 30);
+        }
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
+    }
+
+    public Rigidbody GetRigidBody()
+    {
+        return rb;
     }
 }
